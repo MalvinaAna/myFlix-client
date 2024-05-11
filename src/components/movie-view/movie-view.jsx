fa16d21f31993e978c03ee1c7a3d6eaf0162517b
MalvinaAna <47119139+MalvinaAna@users.noreply.github.com>
2024-05-11 15:58:05 +0300

    created the MainView, MovieCard and MovieView components

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,46 @@
+export const MovieView= ({ movie, onBackClick }) => {
+  return (
+    <div>
+      <div>
+        <img src={movie.ImagePath} />
+      </div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.Title}</span>
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movie.Description}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.Genre.Name}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.Director.Name}</span>
+      </div>
+      <div>
+        <span>Bio: </span>
+        <span>{movie.Director.Bio}</span>
+      </div>
+      <div>
+        <span>Birth: </span>
+        <span>{movie.Director.Birth}</span>
+      </div>
+      <div>
+        <span>Death: </span>
+        <span>{movie.Director.Death}</span>
+      </div>
+      <div>
+        <span>Actors: </span>
+        <span>{movie.Actors}</span>
+      </div>
+      <div>
+        <span>Featured: </span>
+        <span>{movie.Featured}</span>
+      </div>
+      <button onClick={onBackClick}>Back</button>
+    </div>
+  );
+};
