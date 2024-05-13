b3f8736d735b1c7329c5900604574f042aa8c478
MalvinaAna <47119139+MalvinaAna@users.noreply.github.com>
2024-05-13 23:10:44 +0300

    used PropTypes for my updated components

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,7 @@
+//Here you import the PropTypes library
+import PropTypes from "prop-types";
+
+//The BookCard function component
 export const MovieCard= ({ movie, onMovieClick }) => {
   return (
     <div
@@ -8,4 +12,31 @@ export const MovieCard= ({ movie, onMovieClick }) => {
       {movie.Title}
     </div>
   );
+};
+
+//Here is where we define all the props constraints for the BookCard
+BookCard.propTypes= {
+  movie: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired
+    }),
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      bio: PropTypes.string.isRequired,
+      birth: PropTypes.string,
+      death: PropTypes.string
+    }),
+    actors: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string
+      })
+    ),
+    featured: PropTypes.bool
+  }).isRequired,
+  onBookClick: PropTypes.func.isRequired,
 };
